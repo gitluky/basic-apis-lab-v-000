9be93de6d3d77b6e71ff85bad660103420ba8f10
Jonathan Luk <jluk87@gmail.com>
2019-08-23 20:57:58 +0000

    Automatically backed up by Learn

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,5 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   get '/search', to: 'repositories#search'
   post '/search', to: 'repositories#github_search'
-  get '/github/callback', to: 'repositories#github_search'
   root 'repositories#search'
 end
